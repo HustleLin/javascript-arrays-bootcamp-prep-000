47637d4e0637e26352be0ab18ceb4ea23f1822f5
Jonathan Lin <jlin8293@gmail.com>
2018-02-25 02:24:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(array);
   array = array.slice(1)
   return array;
 }
