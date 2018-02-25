765ba782e087ebdbdfee515a958cffc77634ccd4
Jonathan Lin <jlin8293@gmail.com>
2018-02-25 01:44:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,22 @@
 var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
 
 
 
